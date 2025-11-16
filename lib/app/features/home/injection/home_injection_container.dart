a95de4299f73bed18a77c5cf08f38e6a51832922
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2025-11-16 15:55:33 -0300

    feat: create api adapter

--- lib/app/features/home/injection/home_injection_container.dart
+++ lib/app/features/home/injection/home_injection_container.dart
@@ -1,3 +1,4 @@
+import 'package:app_test_fiap/app/core/api/dio_api_adapter.dart';
 import 'package:app_test_fiap/app/core/injector_adapter/injection_adapter.dart';
 import 'package:app_test_fiap/app/features/home/controller/home_cubit.dart';
 import 'package:app_test_fiap/app/features/home/services/products/products_service.dart';
@@ -7,12 +8,16 @@ import 'package:app_test_fiap/app/features/home/services/upload/upload_service_r
 
 class HomeInjectionContainer {
   void call(InjectionAdapter dependency) {
+     dependency.registerFactory<DioClientApiAdapter>(
+      DioClientApiAdapter(baseUrl: 'https://api.escuelajs.co'),
+    );
+
     dependency.registerFactory<ProductsService>(
-      ProductsServiceRemote(),
+      ProductsServiceRemote(client: dependency.get<DioClientApiAdapter>()),
     );
 
     dependency.registerFactory<UploadService>(
-      UploadServiceRemote(),
+      UploadServiceRemote(client: dependency.get<DioClientApiAdapter>()),
     );
 
     dependency.registerLazySingleton<HomeCubit>(
